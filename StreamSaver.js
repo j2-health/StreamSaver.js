f53c715f859c91ae5d4ad7bb52fe31a479d0e91f
Kyle Holzinger <kyle@j2health.com>
2025-06-05 16:19:51 -0400

    [KH] use j2 github url instead of personal

--- StreamSaver.js
+++ StreamSaver.js
@@ -30,7 +30,7 @@
     WritableStream: global.WritableStream || ponyfill.WritableStream,
     supported: true,
     version: { full: '2.0.5', major: 2, minor: 0, dot: 5 },
-    mitm: 'https://jimmywarting.github.io/StreamSaver.js/mitm.html?version=2.0.0'
+    mitm: 'https://j2-health.github.io/StreamSaver.js/mitm.html?version=2.0.0'
   }
 
   /**
